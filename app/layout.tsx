d0d129161b7f9479a9826f54db15f5f4f05100a6
Pease Adeniji <adenijiayomide13@gmail.com>
2023-02-10 08:34:52 +0100

    google next_auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,35 @@
-import SideBar from '../components/SideBar'
-import '../styles/globals.css'
+import { SessionProvider } from "../components/SessionProvrder";
+import { getServerSession } from "next-auth";
+import SideBar from "../components/SideBar";
+import "../styles/globals.css";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
+import Login from "../components/Login";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html>
       <head />
       <body>
-        <div className='flex'>
-          <SideBar />
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ) : (
+            <div className="flex">
+              <div className="bg-[#202123] max-w-xs min-h-screen overflow-y-auto md:min-w-[20rem] sticky">
+                <SideBar />
+              </div>
 
-          <div className='bg-[#343541] flex-1'>{children}</div>
-        </div>
-        </body>
+              <div className="bg-[#343541] flex-1">{children}</div>
+            </div>
+          )}
+        </SessionProvider>
+      </body>
     </html>
-  )
+  );
 }
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import NewChat from './NewChat'
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <div className='p-2 flex flex-col h-screen'>
+        <div className="flex-1">
+            <div>
+                <NewChat />
+
+                <div>
+{/* MOdal */}
+                </div>
+
+                {/* Chat rows */}
+            </div>
+        </div>
+    </div>
   )
 }
 
